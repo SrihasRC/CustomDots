66aa8b5842ecfd5bf6ffb73e03c9a8f91d4db88d
Srihas Reddy Challa <challasrihasreddy@gmail.com>
2025-10-16 01:36:20 +0530

    feat: add line no.

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -4,4 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.opt.clipboard = "unnamedplus"
+vim.o.number = true
+vim.o.relativenumber = false
+vim.o.signcolumn = "yes"
 
